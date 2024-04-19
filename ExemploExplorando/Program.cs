439d9adf03b517d466197a35e2828f6516530ccc
JaimeMoreira <jaime.moreira1995@outlook.com>
2024-04-19 14:03:42 -0300

    Inclusão da pasta do programa de exemplo

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Jaime", "Moreira dos Santos Junior");
+Pessoa p2 = new Pessoa("Antônio", "Ferreira dos Santos");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Jaime";
+// p1.Sobrenome = "Moreira";
+// p1.Idade = 28;
+// p1.Apresentar();
